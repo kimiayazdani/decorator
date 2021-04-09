78f0bb600ec4234ed6795ea86b7af1f9b1402399
Kim <yazdanikimia@yahoo.com>
2021-04-09 16:57:23 +0430

    Mocha added

---
+++ decorator/Mocha.java
@@ -0,0 +1,14 @@
+public class Mocha extends Flavour {
+	private Beverage beverage;
+
+	public Mocha(Beverage beverage) {
+		this.beverage = beverage;
+		this.description = this.beverage.getDescription() + " with mocha";
+	}
+
+
+	@Override
+	public double cost() {
+		return this.beverage.cost() + 0.2;
+	}
+}
