ec3eea14e2308c094d39b2fd5ee289352e8437c2
parsa <parsa.eskandar@gmail.com>
2021-04-09 16:43:01 +0430

    Implement HouseBlend class and bug fixes on Beverage class

---
+++ decorator/HouseBlend.java
@@ -0,0 +1,10 @@
+public class HouseBlend extends Beverage{
+    public HouseBlend(){
+        this.description = "Delicious HouseBlend";
+    }
+
+    @Override
+    public double cost() {
+        return 0.89;
+    }
+}
